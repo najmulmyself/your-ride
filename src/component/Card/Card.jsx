f8960aa4c22cac73af4de49e45ae7b0dd2e63898
Najmul Huda <najmul.myself@gmail.com>
2021-05-07 19:05:28 +0600

    destination and useHistory added

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Destination from '../Destination/Destination';
 import './Card.css'
 
 const Card = ({eachRide}) => {
     const {img,name} = eachRide;
+    const history = useHistory();
+    const handleCard = () => {
+        history.push('/destination')
+    }
     return (
-        <div className='card-design'>
+        <div onClick={handleCard} className='card-design'>
             <img src={img} style={{height:'80px'}} alt="" srcset=""/>
             <h3>{name}</h3>
         </div>
--- src/component/Destination/Destination.jsx
+++ src/component/Destination/Destination.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import './Destination.css'
+import map from '../../images/Map.png'
 const Destination = () => {
   return (
-    <div>
-      <h1>This is Destination</h1>
-      {console.log("Destination page")}
-    </div>
+    <Container className="destination-style">
+      <div className='route-div'>
+        <form>
+          <h6>Pick From</h6>
+          <input className='input' type="text" name="from" id=""/>
+          <br/>
+          <br/>   
+          <h6>Pick To</h6>
+          <input className='input' type="text" name="to" id=""/>
+          <br/>
+          <input className="btn-danger mt-3 w-100" type="submit" value="Search"/>
+        </form>
+      </div>
+      <div>
+        <img className='map px-5' src={map} alt="map"/>
+      </div>
+    </Container>
   );
 };
 
