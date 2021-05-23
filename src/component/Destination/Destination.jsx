11cda792e20cde070179b0c8cad7b5987ababcaf
Najmul Huda <najmul.myself@gmail.com>
2021-05-24 00:55:00 +0600

    design added

--- src/component/Destination/Destination.jsx
+++ src/component/Destination/Destination.jsx
@@ -5,26 +5,18 @@ import map from "../../images/Map.png";
 import { useParams } from "react-router";
 import { useForm } from "react-hook-form";
 const Destination = () => {
-
   const { register, handleSubmit } = useForm();
-  const [pickFrom, setPickFrom] = useState('');
-  const [pickTo, setPickTo] = useState('');
-  const [isSearch,setIsSearch] = useState(true);
-  const onSubmit = data => {
-    // setPickFrom(data.From = '')
-    // setPickTo(data.To = '')
-    // const {From , To }
-    if(data.From && data.To){
-      // console.log(isSearch)
-      setPickFrom(data.From)
-      setPickTo(data.To)
-      setIsSearch(false)
+  const [pickFrom, setPickFrom] = useState("");
+  const [pickTo, setPickTo] = useState("");
+  const [isSearch, setIsSearch] = useState(true);
+  const onSubmit = (data) => {
+    if (data.From && data.To) {
+      setPickFrom(data.From);
+      setPickTo(data.To);
+      setIsSearch(false);
     }
   };
-  console.log(isSearch)
   let { id } = useParams();
-  // console.log(id);
-  // fdjlksdjfkjlksdfjljdfs
 
   // const handlePick = (event) => {
   //   let text = event.target.value;
@@ -36,43 +28,51 @@ const Destination = () => {
   //   event.target.reset();
   //   event.preventDefault();
   // };
-  // const isClicked = () => console.log('clicked')
   return (
     <Container className="destination-style">
       <div className="route-div">
-        {isSearch ? <form onSubmit={handleSubmit(onSubmit)}>
-          <h6>Pick From</h6>
-          <input
-            {...register("From")}
-            required
-            className="input"
-            type="text"
-            // name="From"
-            id=""
-            // onBlur={(e) => setPickFrom(e.target.value)}
-          />
-          <br />
-          <br />
-          <h6>Pick To</h6>
-          <input
-            {...register("To")}
-            required
-            className="input"
-            type="text"
-            // name="To"
-            id=""
-            // onBlur={(e) => setPickTo(e.target.value)}
-          />
-          <br />
-          <input
-            // onClick={handleSearch}
-            className="btn-danger mt-3 w-100"
-            type="submit"
-            // isClicked
-            value="Search"
-          />
-        </form> : <div> <p>{pickFrom}</p>
-      <p>{pickTo}</p> </div> }
+        {isSearch ? (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h6>Pick From</h6>
+            <input
+              {...register("From")} // used this for specification || this returns an object which contains value of 'From'
+              required
+              className="input"
+              type="text"
+              // name="From"  // alternative added {...register("example")}
+              id=""
+              // onBlur={(e) => setPickFrom(e.target.value)}
+            />
+            <br />
+            <br />
+            <h6>Pick To</h6>
+            <input
+              {...register("To")} // used this for specification || this returns an object which contains value of 'To'
+              required
+              className="input"
+              type="text"
+              // name="To"
+              id=""
+              // onBlur={(e) => setPickTo(e.target.value)}
+            />
+            <br />
+            <input
+              className="btn-danger mt-5 w-100"
+              type="submit"
+              value="Search"
+            />
+          </form>
+        ) : (
+          <div className="pick-route">
+            <p><FontAwesomeIcon icon={faCoffee} /> {pickFrom}</p>
+            <p>{pickTo}</p>
+            <div>
+            <p>something</p>
+            <p>something</p>
+            <p>something</p>
+            </div>
+          </div>
+        )}
       </div>
       <div>
         <img className="map px-5" src={map} alt="map" />
