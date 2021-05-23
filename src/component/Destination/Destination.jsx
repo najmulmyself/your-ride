d6a0650537bbc786c92d0ea5c58adac5cfe73dbe
Najmul Huda <najmul.myself@gmail.com>
2021-05-23 21:40:17 +0600

    react hook form solved

--- src/component/Destination/Destination.jsx
+++ src/component/Destination/Destination.jsx
@@ -1,35 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
-import './Destination.css'
-import map from '../../images/Map.png'
+import "./Destination.css";
+import map from "../../images/Map.png";
 import { useParams } from "react-router";
+import { useForm } from "react-hook-form";
 const Destination = () => {
-  let {id} = useParams();
-  console.log(id);
-  const handlePick = (event) => {
-    let text = event.target.value;
-    console.log(event.target.name ,text);
-  }
-  const handleSearch = (event) => {
 
-    event.preventDefault();
-  }
+  const { register, handleSubmit } = useForm();
+  const [pickFrom, setPickFrom] = useState('');
+  const [pickTo, setPickTo] = useState('');
+  const [isSearch,setIsSearch] = useState(true);
+  const onSubmit = data => {
+    // setPickFrom(data.From = '')
+    // setPickTo(data.To = '')
+    // const {From , To }
+    if(data.From && data.To){
+      // console.log(isSearch)
+      setPickFrom(data.From)
+      setPickTo(data.To)
+      setIsSearch(false)
+    }
+  };
+  console.log(isSearch)
+  let { id } = useParams();
+  // console.log(id);
+  // fdjlksdjfkjlksdfjljdfs
+
+  // const handlePick = (event) => {
+  //   let text = event.target.value;
+  //   console.log(text)
+  //   event.target.name === 'From' ? setPickFrom(text) : setPickTo(text);
+  //   // console.log(event.target.name, text);
+  // };
+  // const handleSearch = (event) => {
+  //   event.target.reset();
+  //   event.preventDefault();
+  // };
+  // const isClicked = () => console.log('clicked')
   return (
     <Container className="destination-style">
-      <div className='route-div'>
-        <form>
+      <div className="route-div">
+        {isSearch ? <form onSubmit={handleSubmit(onSubmit)}>
           <h6>Pick From</h6>
-          <input className='input' type="text" name="from" id="" onBlur={handlePick}/>
-          <br/>
-          <br/>   
+          <input
+            {...register("From")}
+            required
+            className="input"
+            type="text"
+            // name="From"
+            id=""
+            // onBlur={(e) => setPickFrom(e.target.value)}
+          />
+          <br />
+          <br />
           <h6>Pick To</h6>
-          <input className='input' type="text" name="to" id="" onBlur={handlePick}/>
-          <br/>
-          <input onClick={handleSearch} className="btn-danger mt-3 w-100" type="submit" value="Search"/>
-        </form>
+          <input
+            {...register("To")}
+            required
+            className="input"
+            type="text"
+            // name="To"
+            id=""
+            // onBlur={(e) => setPickTo(e.target.value)}
+          />
+          <br />
+          <input
+            // onClick={handleSearch}
+            className="btn-danger mt-3 w-100"
+            type="submit"
+            // isClicked
+            value="Search"
+          />
+        </form> : <div> <p>{pickFrom}</p>
+      <p>{pickTo}</p> </div> }
       </div>
       <div>
-        <img className='map px-5' src={map} alt="map"/>
+        <img className="map px-5" src={map} alt="map" />
       </div>
     </Container>
   );
--- src/component/Navbar/Navabar.jsx
+++ src/component/Navbar/Navabar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import './Navbar.css'
+import "./Navbar.css";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 const Navabar = () => {
   return (
-    <Navbar className='bg-transparent navbar-style' expand="lg">
+    <Navbar className="bg-transparent navbar-style" expand="lg">
       <Container>
-        <Navbar.Brand href="#home" className='navbar-brand-style'>Your Ride</Navbar.Brand>
+        <Navbar.Brand href="#home" className="navbar-brand-style">
+          Your Ride
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
@@ -16,7 +18,9 @@ const Navabar = () => {
             <Nav.Link href="#link">Destination</Nav.Link>
             <Nav.Link href="#link">Blog</Nav.Link>
             <Nav.Link href="#link">Contact</Nav.Link>
-            <Button variant="danger" className='nav-btnp'>Log In</Button>
+            <Button variant="danger" className="nav-btnp">
+              Log In
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
